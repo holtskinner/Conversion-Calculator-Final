0d944a5cf0c5ef1c8ac494650432476200a21dae
Holt Skinner <HoltWasHere@gmail.com>
2017-11-04 16:09:22 -0500

    Initial Commit

---
+++ Conversion Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Conversion Calculator
+//
+//  Created by Holt Skinner on 11/4/17.
+//  Copyright © 2017 Holt Skinner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
